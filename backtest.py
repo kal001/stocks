928656dff0c879956ecb3c9f4ee7250d98cec136
Fernando Lourenco <fernando.lourenco@lourenco.eu>
2016-06-18 17:04:28 +0100

    2016-06-17 backtest: created option in command line to select stock to test and start and finish dates

--- backtest.py
+++ backtest.py
@@ -30,137 +30,154 @@ TAXONDIVIDENDS = 0.26
 COMISSION = 14.95* 1.04
 ##########################################################################
 
- # Read config file
-parser = SafeConfigParser()
+def main():
+    # Read config file
+    parser = SafeConfigParser()
 
-# Open the file with the correct encoding
-with codecs.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), SETTINGSFILE), 'r', encoding='utf-8') as f:
-    parser.readfp(f)
+    # Open the file with the correct encoding
+    with codecs.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), SETTINGSFILE), 'r', encoding='utf-8') as f:
+        parser.readfp(f)
 
-global DATABASE
-DATABASE = parser.get('Database', 'File')
+    global DATABASE
+    DATABASE = parser.get('Database', 'File')
 
 
-cloptions = argparse.ArgumentParser(description='Backtest vs. %s' % version.__version__, prog='Backtest %s' % version.__version__)
-cloptions.add_argument('-s', '--stock', help='Yahoo finance symbol of stock to backtest', nargs = 1, default = ['QLIK'],required=True)
-cloptions.add_argument('-t', '--sdate', help='Start date to backtest (format yyyy-mm-dd)', nargs = 1, default = ['2012-01-01'],required=False)
-cloptions.add_argument('-f', '--fdate', help='Finish date to backtest (format yyyy-mm-dd)', nargs = 1,required=False)
+    cloptions = argparse.ArgumentParser(description='Backtest vs. %s' % version.__version__, prog='Backtest %s' % version.__version__)
+    cloptions.add_argument('-s', '--stock', help='Yahoo finance symbol(s) of stock to backtest (comma separated)', nargs = 1, default = ['QLIK'],required=True)
+    cloptions.add_argument('-t', '--sdate', help='Start date to backtest (format yyyy-mm-dd)', nargs = 1, default = ['2012-01-01'],required=False)
+    cloptions.add_argument('-f', '--fdate', help='Finish date to backtest (format yyyy-mm-dd)', nargs = 1,required=False)
+    cloptions.add_argument('-l', '--lowcount', help='Strategy low count', nargs = 1, default = 5, required=False)
+    cloptions.add_argument('-m', '--minreturn', help='Strategy Minimum return (%)', nargs = 1, default = 5, required=False)
 
-cloptions.add_argument('-v', '--version', action='version', version='%(prog)s')
+    cloptions.add_argument('-v', '--version', action='version', version='%(prog)s')
 
-args = cloptions.parse_args()
-
-try:
-    stocksymbol = args.stock[0]
-except:
-    exit(1)
-
-try:
-    startdate = args.sdate[0]
-except:
-    startdate = "2012-01-01"
-
-try:
-    finishdate = args.fdate[0]
-except:
-    finishdate = (datetime.datetime.utcnow() + datetime.timedelta( days=-1 )).strftime("%Y-%m-%d")
-
-if VERBOSE:
-    print "Start %s\n%s\n%s\n\n" % (os.path.basename(sys.argv[0]), version.__version__, datetime.datetime.now())
-
-conn = sqlite3.connect(DATABASE)
-conn.row_factory = sqlite3.Row
-
-c = conn.cursor()
-c.execute("select symbolyahoo from stocks where type='stock' and symbolyahoo=:symbol;", {'symbol':stocksymbol})
-stocks = c.fetchall()
-
-for STOCK in stocks:
-
-    CAPITAL = 10000
+    args = cloptions.parse_args()
 
     try:
-        quotes = get_historical_prices(STOCK[0], startdate, finishdate)
+        stocksymbol = args.stock[0]
     except:
-        print "Error geting quotes for %s from %s to %s" % (stocksymbol, startdate, finishdate)
         exit(1)
 
-    sortedquotes = sorted(quotes.items(), key=operator.itemgetter(0))
+    try:
+        startdate = args.sdate[0]
+    except:
+        startdate = "2012-01-01"
 
-    # get stock id
-    c.execute("select id from stocks where symbolyahoo='%s';" % STOCK[0])
-    stockid = c.fetchone()[0]
+    try:
+        finishdate = args.fdate[0]
+    except:
+        finishdate = (datetime.datetime.utcnow() + datetime.timedelta( days=-1 )).strftime("%Y-%m-%d")
 
-    # get dividends
-    c.execute("select * from dividends where stockid=%d;" % stockid)
-    dividends = c.fetchall()
-    datesdividends=[o['date'] for o in dividends]
+    try:
+        LOWCOUNT = args.lowcount
+    except:
+        pass
 
-    quoteant = 0.0
-    finalquote="0.0"
-    countlow = 0
+    try:
+        MINRETURN = float(args.minreturn)/100
+    except:
+        pass
 
-    buy = False
-    qty = 0
-    buyprice = 0.0
-    cash = CAPITAL
+    if VERBOSE:
+        print "Start %s\n%s\n%s\n\n" % (os.path.basename(sys.argv[0]), version.__version__, datetime.datetime.now())
 
-    buys = 0
-    sells = 0
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
 
-    for day in sortedquotes:
-        #calculate day return
-        if quoteant != 0:
-            dayreturn = (float(day[1]["Close"])-quoteant)/quoteant
-        else:
-            dayreturn = 0
+    c = conn.cursor()
+    c.execute("select symbolyahoo from stocks where type='stock' and symbolyahoo=:symbol;", {'symbol':stocksymbol})
+    stocks = c.fetchall()
 
-        if buy == False:
-            #check number of consecutive days lowering exceeded, and opening low again
-            if countlow>= LOWCOUNT:
-                if float(day[1]["Open"])<quoteant:
-                    buy = True
-                    buyprice = float(day[1]["Open"])
-                    qty = int((cash-COMISSION)/buyprice)
-                    cash = cash - qty * buyprice - COMISSION
+    for STOCK in stocks:
+
+        CAPITAL = 10000
+
+        try:
+            quotes = get_historical_prices(STOCK[0], startdate, finishdate)
+        except:
+            print "Error geting quotes for %s from %s to %s" % (stocksymbol, startdate, finishdate)
+            exit(1)
+
+        sortedquotes = sorted(quotes.items(), key=operator.itemgetter(0))
+
+        # get stock id
+        c.execute("select id from stocks where symbolyahoo='%s';" % STOCK[0])
+        stockid = c.fetchone()[0]
+
+        # get dividends
+        c.execute("select * from dividends where stockid=%d;" % stockid)
+        dividends = c.fetchall()
+        datesdividends=[o['date'] for o in dividends]
+
+        quoteant = 0.0
+        finalquote="0.0"
+        countlow = 0
+
+        buy = False
+        qty = 0
+        buyprice = 0.0
+        cash = CAPITAL
+
+        buys = 0
+        sells = 0
+
+        for day in sortedquotes:
+            #calculate day return
+            if quoteant != 0:
+                dayreturn = (float(day[1]["Close"])-quoteant)/quoteant
+            else:
+                dayreturn = 0
+
+            if buy == False:
+                #check number of consecutive days lowering exceeded, and opening low again
+                if countlow>= LOWCOUNT:
+                    if float(day[1]["Open"])<quoteant:
+                        buy = True
+                        buyprice = float(day[1]["Open"])
+                        qty = int((cash-COMISSION)/buyprice)
+                        cash = cash - qty * buyprice - COMISSION
+                        if VERBOSE:
+                            print("BOUGHT %8.0f @ %8.3f" % (qty, buyprice))
+
+                        buys += 1
+            else:
+                # Check if minimum return acheived, so sell
+                if (float(day[1]["Open"])-buyprice)/buyprice>=MINRETURN:
+                    buy = False
+                    cash = cash + qty * float(day[1]["Open"]) - COMISSION
+                    qty = 0
                     if VERBOSE:
-                        print("BOUGHT %8.0f @ %8.3f" % (qty, buyprice))
+                        print("SOLD! Cash now:%6.0f" % cash)
 
-                    buys += 1
+                    sells += 1
+
+            if buy == True:
+                # check to see if dividend paid
+                if day[0] in datesdividends:
+                    i = datesdividends.index(day[0])
+                    cash = cash + qty * dividends[i]['value'] * (1-TAXONDIVIDENDS)
+                    if VERBOSE:
+                        print "Dividend pay %s * %d" % (dividends[i]['value'],qty)
+
+            #increase number of consecutive days lowering
+            if dayreturn<0.0:
+                countlow = countlow+1
+            else:
+                countlow = 0
+
+            quoteant = float(day[1]["Close"])
+            if VERBOSE:
+                print("Date: %s Close Price:%s Adj Close:%s Change:%5.2f%%" % (day[0], day[1]["Close"], day[1]["Adj Close"], dayreturn*100) )
+            finalquote = day[1]["Close"]
+
+        print("\nStock %s" % STOCK[0])
+        if buy == False:
+            print("Final Cash:%6.0f" % cash)
         else:
-            # Check if minimum return acheived, so sell
-            if (float(day[1]["Open"])-buyprice)/buyprice>=MINRETURN:
-                buy = False
-                cash = cash + qty * float(day[1]["Open"]) - COMISSION
-                qty = 0
-                if VERBOSE:
-                    print("SOLD! Cash now:%6.0f" % cash)
+            print("Final Position %8.0f stock valued %6.0f" % (qty, qty*float(finalquote)))
 
-                sells += 1
+        print ("Total Buys:%d Total Sells:%d" % (buys, sells))
 
-        if buy == True:
-            # check to see if dividend paid
-            if day[0] in datesdividends:
-                i = datesdividends.index(day[0])
-                cash = cash + qty * dividends[i]['value'] * (1-TAXONDIVIDENDS)
-                if VERBOSE:
-                    print "Dividend pay %s * %d" % (dividends[i]['value'],qty)
-
-        #increase number of consecutive days lowering
-        if dayreturn<0.0:
-            countlow = countlow+1
-        else:
-            countlow = 0
-
-        quoteant = float(day[1]["Close"])
-        if VERBOSE:
-            print("Date: %s Close Price:%s Adj Close:%s Change:%5.2f%%" % (day[0], day[1]["Close"], day[1]["Adj Close"], dayreturn*100) )
-        finalquote = day[1]["Close"]
-
-    print("\nStock %s" % STOCK[0])
-    if buy == False:
-        print("Final Cash:%6.0f" % cash)
-    else:
-        print("Final Position %8.0f stock valued %6.0f" % (qty, qty*float(finalquote)))
-
-    print ("Total Buys:%d Total Sells:%d" % (buys, sells))
+##########################################################################
+if __name__ == "__main__":
+    main()
